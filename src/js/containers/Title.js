7d10393710633f295b8d1a9dc8aadfb3099ee0d9
Calvin Choi <hyunjinchoi@Calvins-MacBook-Air.local>
2019-10-13 00:33:04 -0400

    Set font sizes

--- src/js/containers/Title.js
+++ src/js/containers/Title.js
@@ -5,14 +5,14 @@ class Title extends Component {
   render() {
     return (
       <div className="title row">
-        <div className="column align-left">
+        <div className="column align-left font-small">
           Toronto, ON<br />
           www.ccalvin.com
         </div>
-        <div className="column align-center bold small-caps">
-          CALVIN CHOI
+        <div className="column align-center small-caps bold font-large">
+          Calvin Choi
         </div>
-        <div className="column align-right">
+        <div className="column align-right font-small">
           (647) 673-5633<br />
           calvinhyunchoi@gmail.com
         </div>
