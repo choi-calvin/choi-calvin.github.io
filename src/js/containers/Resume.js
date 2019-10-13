ceeb62a27865050fc63415c44b6e3d6cf96f55fb
Calvin Choi <hyunjinchoi@Calvins-MacBook-Air.local>
2019-10-13 00:03:17 -0400

    Add bodies

--- src/js/containers/Resume.js
+++ src/js/containers/Resume.js
@@ -3,6 +3,7 @@ import './Resume.css';
 import Title from './Title';
 import Header from './Header';
 import Subheader from './Subheader';
+import Bodies from '../data/bodies';
 
 class Resume extends Component {
   render() {
@@ -16,34 +17,42 @@ class Resume extends Component {
             textCenter="University of Toronto"
             textRight="September 2016 – May 2021"
           />
+          {Bodies.educationMain}
           <Header text="Employment" />
           <Subheader
             textLeft="Machine Learning Developer"
             textCenter="Dessa"
             textRight="September 2019 – Present"
           />
+          {Bodies.employmentDessa}
           <Subheader
             textLeft="Junior Software Developer"
             textCenter="PolicyMe"
             textRight="May – July 2019"
           />
+          {Bodies.employmentPolicyMe}
           <Subheader
             textLeft="Teaching Assistant"
             textCenter="University of Toronto"
             textRight="January – April 2019"
           />
+          {Bodies.employmentTA}
           <Subheader
             textLeft="Android Developer"
             textCenter="Computational Cognitive Development Lab"
             textRight="September 2018 – April 2019"
           />
+          {Bodies.employmentCoCo}
           <Subheader
             textLeft="Lab Data Scientist"
             textCenter="Duncan Lab"
             textRight="May – August 2018"
           />
+          {Bodies.employmentDuncan}
           <Header text="Projects" />
+          {Bodies.projectsMain}
           <Header text="Languages and Technologies" />
+          {Bodies.languagesMain}
         </div>
       </div>
     );
---
+++ src/js/data/bodies.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const Bodies = {
+  educationMain: (
+    <p>
+      •	B.S. in Computer Science and Cognitive Science, expected May 2021. <b>GPA: 3.85</b><br />
+      •	Coursework: Operating Systems; Databases; Machine Learning; Artificial Intelligence; Web Development; Cognitive Neuroscience
+    </p>
+  ),
+  employmentDessa: (
+    <p>
+      •	Developed a Python SDK to save machine learning model artifacts to redis using Flask and Docker, used by <b>hundreds of AI researchers</b>.<br />
+      •	Integrated graphing libraries into the user interface using React to visualize parameter-metric correlations from machine learning models.<br />
+      •	Managed automated builds for <b>product releases</b> via Docker, Jenkins, and Google Container Registry.
+    </p>
+  ),
+  employmentPolicyMe: (
+    <p>
+      •	Created an end-to-end RPA pipeline using React.js, PostgreSQL, Python, Selenium, and AWS Elastic Beanstalk to automate form submissions and <b>save 20+ hours</b> of weekly manual labour.<br />
+      •	Integrated email validation using React.js, Redux, and Kickbox to prevent fraudulent information from polluting the data pipeline.
+    </p>
+  ),
+  employmentTA: (
+    <p>
+      •	Facilitated learning of an introductory computer science course by <b>leading a class</b> of over 30 students.<br />
+      •	<b>Taught</b> fundamental concepts on data structures, algorithms, and object-oriented programming.
+    </p>
+  ),
+  employmentCoCo: (
+    <p>
+      •	Designed and developed an Android application to manage psychological experiments for use in current <b>world-leading cognitive research</b>, integrating computer vision models using TensorFlow Lite.
+    </p>
+  ),
+  employmentDuncan: (
+    <p>
+      •	Rewrote a part of the pipeline for eye-tracking experiment data using pandas to improve speed efficiency of data flow by <b>10x</b>.<br />
+      •	Managed data collection using Amazon’s MTurk and the AWS SDK.
+    </p>
+  ),
+  projectsMain: (
+    <p>
+      •	<b>ext2:</b> Implementation of the ext2 file system from scratch, supporting file system functions such as navigation, directory creation, file linking, and corruption detection and restoral.  <i>C</i><br />
+      •	<b>Color Infinity:</b> iOS game monetized with Google AdMob. Accumulated 100+ installs and a 5-star rating on the App Store.  <i>Swift, Xcode</i><br />
+      •	<b>DontSkipTheDishes:</b> Online dashboard of personalized food recipes based on ingredients in your fridge. Users could create accounts to log their ingredients and rate other people’s recipes.  <i>React.js, Express.js, MongoDB</i>
+    </p>
+  ),
+  languagesMain: (
+    <p>
+      •	Python, React.js, Javascript, Java, C (proficient)<br />
+      •	Bash, Docker (knowledgeable)<br />
+      •	Swift, Xcode, Android Studio (prior experience)<br />
+      •	Git, particularly with GitHub
+    </p>
+  ),
+};
+
+export default Bodies;
