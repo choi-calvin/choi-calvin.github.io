f91b060aaa6e15c23d3f89f2554468cebffa746d
Calvin Choi <hyunjinchoi@Calvins-MacBook-Air.local>
2019-10-14 23:03:11 -0400

    Integrate GA, but doesn't seem to be working yet

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import './common.css';
 import Resume from './Resume';
+import ReactGA from 'react-ga';
 
 class App extends Component {
+  componentDidMount() {
+    ReactGA.initialize('UA-150069983-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   render() {
     return (
       <div className="App">
