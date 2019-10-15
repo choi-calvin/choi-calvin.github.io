636337fff34689e4ede579e740baeca941adeea2
Calvin Choi <hyunjinchoi@Calvins-MacBook-Air.local>
2019-10-14 23:02:53 -0400

    Changes to cogsci tooltip

--- src/js/containers/Tooltips.js
+++ src/js/containers/Tooltips.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactTooltip from 'react-tooltip';
-import Cogsci from '../../resources/images/cogsci.svg';
+import Cogsci from '../../resources/images/cogsci.png';
 import './Tooltips.css';
 
 class Tooltips extends Component {
@@ -10,7 +10,8 @@ class Tooltips extends Component {
         <ReactTooltip place="bottom" type="dark" effect="solid" className="react-tooltip"/>
         <ReactTooltip id="cogsci" place="bottom" type="dark" effect="solid" className="react-tooltip">
           I strongly believe that ideas from psychology and neuroscience can benefit current AI research.
-          A lot of my non-technical coursework revolves around how the brain forms neural connections to learn new things.<br />
+          A lot of my non-technical coursework revolves around how the brain forms neural connections to learn new things.<br /><br />
+          This cultish-looking diagram illustrates the essence of cognitive science:<br />
           <img src={Cogsci} className="cogsci-img" alt="A diagram showing the disciplines of cognitive science." />
         </ReactTooltip>
         <ReactTooltip id="dessa" place="bottom" type="dark" effect="solid" className="react-tooltip">
