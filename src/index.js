737a82b200a9b8625b0f6787df10a4b95019a758
Calvin Choi <hyunjinchoi@Calvins-MacBook-Air.local>
2019-10-14 23:19:55 -0400

    Initialize GA before sending pageview..

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './js/containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-150069983-1');
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-ReactGA.initialize('UA-150069983-1');
