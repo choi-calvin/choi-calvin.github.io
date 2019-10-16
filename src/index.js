644ec6fe39523a4de02cc2b7ad793d1a4fcd0958
Calvin Choi <hyunjinchoi@Calvins-MacBook-Air.local>
2019-10-15 20:41:12 -0400

    Integrated GA as env var in prod

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './js/containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize(process.env.GA);
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
